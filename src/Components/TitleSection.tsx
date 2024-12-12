2922dd1acd1a3c01ffcdc3765f5b0fc570ce6f73
donado10 <adamscoulibaly10@gmail.com>
2024-12-12 21:47:50 +0000

    bugs

--- src/Components/TitleSection.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TitleSection: React.FC<{ title: string }> = ({ title }) => {
-  return <h1 className="text-2xl font-normal text-white">{title}</h1>;
-};
-
-export default TitleSection;
